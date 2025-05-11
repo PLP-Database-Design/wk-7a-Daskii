dccefd10e5f0cdff881855a246dd27eb85b51c13
Mpendulo Maduna <108370062+Daskii@users.noreply.github.com>
2025-05-11 20:25:12 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -14,3 +14,25 @@ ORDER BY
 
 
 #QUESTION 2
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(255)
+);
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
